e7d2dda369fe43f764e322d3bdd3ce638f172537
strengur <steingrimur@740.is>
2017-03-14 22:05:53 +0000

    Styling and Spotify API modification

--- js/app.js
+++ js/app.js
@@ -1,24 +1,51 @@
 // Points for API's selections navigation
   // Add selected class to selected API
-
+var spotifyButton = document.getElementById('spotify-button');
 
 // Spotify feed
-  var spotifyAPI = "https://api.spotify.com/v1/search?type=playlist&q=focus&limit=6";
-  var spotifyPlaylist;
+function spotify(e) {
+  var limit = e;
+  var spotifyAPI = "https://api.spotify.com/v1/search?";
+  var spotifyOptions =
+  {
+    type : "playlist",
+    q : "focus",
+    limit : limit
+  };
   function displayPlaylists(data) {
     var playlistHTML = '<ul class="inline-flex-container">';
-    console.log(data.playlists.items[0].external_urls.spotify);
     $.each(data.playlists.items, function (i, playlists) {
-
       playlistHTML += '<li class="inline-flex-item">' + playlists.name;
       playlistHTML += '<a href="' + data.playlists.items[i].external_urls.spotify + '">';
       playlistHTML += '<img src="' + data.playlists.items[i].images[0].url + '" alt="' + playlists.name + '">';
       playlistHTML += '</a></li>';
-    }), 5000;
+    });
     playlistHTML += '</ul>';
     $('#items-listing').html(playlistHTML);
+
   }
+  $.getJSON(spotifyAPI, spotifyOptions, displayPlaylists);
+}
 
-  $.getJSON(spotifyAPI, displayPlaylists);
+spotifyButton.addEventListener('click', function() {
+  spotify(6);
+  var listingsName = this.innerText;
+  $('#listings-name').html(listingsName);
+});
+// OMDB feed
+  var omdbAPI = "http://www.omdbapi.com/?"
+  var omdbOptions =
+  [
+    {
+      t : "shooter",
+      tomatoes: "true"
+    },
+    {
+      t : "Bad Boys"
+    }
+  ]
+  function omdbPosters(data) {
+    console.log(data);
 
-// Flickr feed
+  }
+  $.getJSON(omdbAPI, omdbOptions[1], omdbPosters);
